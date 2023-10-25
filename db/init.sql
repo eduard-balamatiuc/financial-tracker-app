ead00b08ac498c9b90ee3fc88c6b34cfacda1aee
pyramixofficial <balamatiuc2@gmail.com>
2023-10-26 00:47:08 +0300

    adapted the sql structure

--- db/init.sql
+++ db/init.sql
@@ -1,21 +1,21 @@
-﻿CREATE TABLE "users" (
+﻿CREATE TABLE "user" (
     "user_ID" int   NOT NULL,
     "user_name" varchar(50)   NOT NULL,
     "email" varchar(100)   NOT NULL,
     "password_hash" varchar(255)   NOT NULL,
-    CONSTRAINT "pk_Users" PRIMARY KEY (
+    CONSTRAINT "pk_user" PRIMARY KEY (
         "user_ID"
      ),
-    CONSTRAINT "uc_Users_user_name" UNIQUE (
+    CONSTRAINT "uc_user_user_name" UNIQUE (
         "user_name"
     ),
-    CONSTRAINT "uc_Users_email" UNIQUE (
+    CONSTRAINT "uc_user_email" UNIQUE (
         "email"
     )
 );
 
-CREATE TABLE "expenses" (
-    "expenses_ID" int   NOT NULL,
+CREATE TABLE "expense" (
+    "expense_ID" int   NOT NULL,
     "user_ID" int   NOT NULL,
     "amount" decimal   NOT NULL,
     "currency" varchar   NOT NULL,
@@ -24,15 +24,15 @@ CREATE TABLE "expenses" (
     "p_date" date   NOT NULL,
     "payment_method_ID" varchar   NOT NULL,
     "card_type" varchar   NOT NULL,
-    CONSTRAINT "pk_Expenses" PRIMARY KEY (
-        "expenses_ID"
+    CONSTRAINT "pk_expense" PRIMARY KEY (
+        "expense_ID"
      )
 );
 
 CREATE TABLE "category" (
     "category_ID" varchar   NOT NULL,
     "category_name" varchar   NOT NULL,
-    CONSTRAINT "pk_Category" PRIMARY KEY (
+    CONSTRAINT "pk_category" PRIMARY KEY (
         "category_ID"
      )
 );
@@ -42,17 +42,17 @@ CREATE TABLE "payment_method" (
     "c_type" varchar   NOT NULL,
     "number" decimal   NOT NULL,
     "bank" varchar   NOT NULL,
-    CONSTRAINT "pk_Payment_method" PRIMARY KEY (
+    CONSTRAINT "pk_payment_method" PRIMARY KEY (
         "payment_method_ID"
      )
 );
 
-ALTER TABLE "expenses" ADD CONSTRAINT "fk_Expenses_user_ID" FOREIGN KEY("user_ID")
-REFERENCES "users" ("user_ID");
+ALTER TABLE "expense" ADD CONSTRAINT "fk_expense_user_ID" FOREIGN KEY("user_ID")
+REFERENCES "user" ("user_ID");
 
-ALTER TABLE "expenses" ADD CONSTRAINT "fk_Expenses_category_ID" FOREIGN KEY("category_ID")
+ALTER TABLE "expense" ADD CONSTRAINT "fk_expense_category_ID" FOREIGN KEY("category_ID")
 REFERENCES "category" ("category_ID");
 
-ALTER TABLE "expenses" ADD CONSTRAINT "fk_Expenses_payment_method_ID" FOREIGN KEY("payment_method_ID")
+ALTER TABLE "expense" ADD CONSTRAINT "fk_expense_payment_method_ID" FOREIGN KEY("payment_method_ID")
 REFERENCES "payment_method" ("payment_method_ID");
 
--- db/populate_data.sql
+++ db/populate_data.sql
@@ -1,5 +1,5 @@
 -- Insert sample data into Users table
-INSERT INTO "users" ("user_ID", "user_name", "email", "password_hash")
+INSERT INTO "user" ("user_ID", "user_name", "email", "password_hash")
 VALUES (1, 'user1', 'user1@example.com', 'password123');
 
 -- Insert sample data into Category table
@@ -11,7 +11,7 @@ INSERT INTO "payment_method" ("payment_method_ID", "c_type", "number", "bank")
 VALUES ('credit_card', 'Credit Card', 1234567890123456, 'Bank of America');
 
 -- Insert sample data into Expenses table
-INSERT INTO "expenses" ("expenses_ID", "user_ID", "amount", "currency", "category_ID", "description", "p_date", "payment_method_ID", "card_type")
+INSERT INTO "expense" ("expense_ID", "user_ID", "amount", "currency", "category_ID", "description", "p_date", "payment_method_ID", "card_type")
 VALUES (1, 1, 110.0, 'USD', 'food', 'Lunch', '2023-10-25', 'credit_card', 'Visa');
 
 
