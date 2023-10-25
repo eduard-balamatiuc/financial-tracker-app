6c3861a69a008b488d81299d31e3288e8128af03
pyramixofficial <balamatiuc2@gmail.com>
2023-10-26 00:45:45 +0300

    removed main.py from back

--- back/main.py
+++
@@ -1,8 +0,0 @@
-from back import app
-from routes import main_routes
-
-# Register the main_routes Blueprint
-app.register_blueprint(main_routes)
-
-if __name__ == '__main__':
-    app.run(debug=True)
